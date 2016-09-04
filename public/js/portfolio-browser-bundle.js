233b600b468020700126b54500e2c6fa8570ddb8
Extra <yyin44@wisc.edu>
2016-09-04 08:59:57 -0500

    add React Class

--- public/js/portfolio-browser-bundle.js
+++ public/js/portfolio-browser-bundle.js
@@ -50,11 +50,120 @@
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
 	
-	ReactDOM.render(React.createElement(
-	  'h1',
-	  null,
-	  'Hello React!'
-	), document.getElementById('portfolio_root'));
+	// ------------------------------
+	var projects = [{
+		project_title: "PAVI_Office_system",
+		project_detail: "say something say something say something say something say something say something say something",
+		project_img: "public/img/PAVI_Office_system.jpg",
+		project_detail_page: "public/img/PAVI_Office_system.jpg"
+	}, {
+		project_title: "加湿器",
+		project_detail: "say something say something say something say something say something say something say something",
+		project_img: "public/img/加湿器.jpg",
+		project_detail_page: "public/img/加湿器.pdf"
+	}, {
+		project_title: "竹编几",
+		project_detail: "say something say something say something say something say something say something say something",
+		project_img: "public/img/竹编几.jpg",
+		project_detail_page: "public/img/竹编几.pdf"
+	}, {
+		project_title: "OVO_STOOL",
+		project_detail: "say something say something say something say something say something say something say something",
+		project_img: "public/img/OVO_STOOL.jpg",
+		project_detail_page: "public/img/OVO_STOOL0.jpg"
+	}, {
+		project_title: "弹电吉他的维特鲁威人",
+		project_detail: "say something say something say something say something say something say something say something",
+		project_img: "public/img/OVO_STOOL2.jpg",
+		project_detail_page: "public/img/OVO_STOOL0.jpg"
+	}];
+	// ------------------------------
+	var PortfolioItem = React.createClass({
+		displayName: 'PortfolioItem',
+		getInitialState: function getInitialState() {
+			return {
+				opacity: 1
+			};
+		},
+		handleHoverOn: function handleHoverOn() {
+			this.setState({ opacity: "0.8" });
+		},
+		handleHoverOff: function handleHoverOff() {
+			this.setState({ opacity: "1" });
+		},
+	
+		render: function render() {
+			return React.createElement(
+				'div',
+				{ className: 'col-md-4 portfolio-item' },
+				React.createElement(
+					'a',
+					{ href: this.props.project['project_detail_page'] },
+					React.createElement(
+						'div',
+						{ className: 'project-img-wrapper' },
+						React.createElement('img', {
+							className: 'img-responsive',
+							src: this.props.project['project_img'],
+							alt: this.props.project['project_title'],
+							style: { opacity: this.state.opacity },
+							onMouseEnter: this.handleHoverOn,
+							onMouseLeave: this.handleHoverOff
+						})
+					)
+				),
+				React.createElement(
+					'h3',
+					null,
+					React.createElement(
+						'a',
+						{ className: 'project_link', href: this.props.project['project_detail_page'] },
+						this.props.project['project_title']
+					)
+				),
+				React.createElement(
+					'p',
+					null,
+					this.props.project['project_detail']
+				)
+			);
+		}
+	});
+	
+	var PortfolioRow = React.createClass({
+		displayName: 'PortfolioRow',
+	
+		render: function render() {
+			var item_row = [];
+			for (var i = 0; i < this.props.projects.length; i++) {
+				item_row.push(React.createElement(PortfolioItem, { project: this.props.projects[i] }));
+			}
+			return React.createElement(
+				'div',
+				{ className: 'row' },
+				item_row,
+				';'
+			);
+		}
+	});
+	
+	var PortfolioDisplay = React.createClass({
+		displayName: 'PortfolioDisplay',
+	
+		render: function render() {
+			var each_row = [];
+			for (var i = 0; i < this.props.projects.length / 3; i++) {
+				each_row.push(React.createElement(PortfolioRow, { projects: this.props.projects.slice(i * 3, (i + 1) * 3) }));
+			}
+			return React.createElement(
+				'div',
+				{ className: 'container' },
+				each_row
+			);
+		}
+	});
+	
+	ReactDOM.render(React.createElement(PortfolioDisplay, { projects: projects }), document.getElementById('portfolio_root'));
 
 /***/ },
 /* 1 */
--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -2,4 +2,99 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('portfolio_root'));
+// ------------------------------
+var projects = [{
+					project_title: "PAVI_Office_system",
+					project_detail: "say something say something say something say something say something say something say something",
+					project_img: "public/img/PAVI_Office_system.jpg",
+					project_detail_page: "public/img/PAVI_Office_system.jpg"
+				}, {
+					project_title: "加湿器",
+					project_detail: "say something say something say something say something say something say something say something",
+					project_img: "public/img/加湿器.jpg",
+					project_detail_page: "public/img/加湿器.pdf"
+				}, {
+					project_title: "竹编几",
+					project_detail: "say something say something say something say something say something say something say something",
+					project_img: "public/img/竹编几.jpg",
+					project_detail_page: "public/img/竹编几.pdf"
+				}, {
+					project_title: "OVO_STOOL",
+					project_detail: "say something say something say something say something say something say something say something",
+					project_img: "public/img/OVO_STOOL.jpg",
+					project_detail_page: "public/img/OVO_STOOL0.jpg"
+				}, {
+					project_title: "弹电吉他的维特鲁威人",
+					project_detail: "say something say something say something say something say something say something say something",
+					project_img: "public/img/OVO_STOOL2.jpg",
+					project_detail_page: "public/img/OVO_STOOL0.jpg"
+				}
+];
+// ------------------------------
+var PortfolioItem = React.createClass({
+    getInitialState() {
+        return {
+            opacity: 1
+        };
+    },
+    handleHoverOn(){
+        this.setState({opacity: "0.8"});
+    },
+    handleHoverOff(){
+        this.setState({opacity: "1"});
+    },
+	render: function () {
+		return (
+			<div className="col-md-4 portfolio-item">
+                <a href={this.props.project['project_detail_page']}>
+                    <div className="project-img-wrapper">
+                        <img
+                            className="img-responsive"
+                            src={this.props.project['project_img']}
+                            alt={this.props.project['project_title']}
+                            style={{opacity: this.state.opacity}}
+                            onMouseEnter={this.handleHoverOn}
+                            onMouseLeave={this.handleHoverOff}
+                        />
+                    </div>
+                </a>
+                <h3>
+                    <a className="project_link" href={this.props.project['project_detail_page']}>{this.props.project['project_title']}</a>
+                </h3>
+                <p>{this.props.project['project_detail']}</p>
+            </div>
+		);
+	}
+});
+
+var PortfolioRow = React.createClass({
+	render: function () {
+		var item_row = [];
+		for (var i=0; i<this.props.projects.length; i++){
+			item_row.push(<PortfolioItem project={this.props.projects[i]} />);
+		}
+		return (
+			<div className="row">
+				{item_row};
+			</div>
+		);
+	}
+});
+
+
+var PortfolioDisplay = React.createClass({
+	render: function () {
+		var each_row = [];
+		for (var i=0; i<this.props.projects.length/3; i++){
+			each_row.push(<PortfolioRow projects={this.props.projects.slice(i*3, (i+1)*3)} />);
+		}
+		return (
+			<div className="container">
+				{each_row}
+			</div>
+		);
+	}
+});
+
+
+ReactDOM.render(<PortfolioDisplay projects={projects}/>, document.getElementById('portfolio_root'));
