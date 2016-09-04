d17359fe7868f1bb653911f9ca5ba89d26b8e8a6
Extra <yyin44@wisc.edu>
2016-09-04 09:04:46 -0500

    fix bug

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -26,7 +26,7 @@ var projects = [{
 				}, {
 					project_title: "弹电吉他的维特鲁威人",
 					project_detail: "say something say something say something say something say something say something say something",
-					project_img: "public/img/OVO_STOOL2.jpg",
+					project_img: "public/img/弹电吉他的维特鲁威人.jpg",
 					project_detail_page: "public/img/OVO_STOOL0.jpg"
 				}
 ];
