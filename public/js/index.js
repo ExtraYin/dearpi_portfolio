36886c03e1b445e64052d4bb6832fa7e0dd3aa76
Extra <yyin44@wisc.edu>
2016-09-04 22:04:00 -0500

    add random bkg image

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,8 @@
 //index.js
+
+var bkg_img_list = ['123665147859405668.jpg', '564206975470062966.jpg', '640999239315762742.jpg', '642937979973547359.jpg',
+    '712012402254975154.jpg', '715252349923378279.jpg', '908364515923191597.jpg', 'index_L2.jpg'];
+
+var selected = Math.floor(Math.random() * bkg_img_list.length);
+
+$(".header_photo").attr("src", "public/img/index_bkg/" + bkg_img_list[selected]);
